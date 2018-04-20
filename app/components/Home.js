31aed22e99c8d7b14dceeee5d526988352d8b203
tannerkaysmith <34726916+tannerkaysmith@users.noreply.github.com>
2018-04-20 11:35:43 -0600

    hosting

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,8 +5,8 @@ class Home extends React.Component {
   render() {
     return (
       <div className='home-container'>
-        <img src="logo.svg" alt="Smiley face" className='logo' />
-        <h1 className='home-h1' >Github Battle: Battle your friends... and stuff.</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="Smiley face" className='logo' />
+        <h1 className='home-h1' >Github Battle: Battle your friends.</h1>
         <Link className='button' to='/battle'>Battle</Link>
       </div>
     )
